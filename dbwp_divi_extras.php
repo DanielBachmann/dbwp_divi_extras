e7ae3a8e6952d0c0277afd2dedc9a09b3baf5952
Daniel Bachmann <danielbachm@gmx.de>
2018-01-18 15:55:31 +0100

    init Just Plugininformations and License

---
+++ dbwp_divi_extras.php
@@ -0,0 +1,24 @@
+/*
+Plugin Name: Custom Module for Divi Builder
+Plugin URI:  <Plugin URI (use Github?)>
+Description: This plugins creates a custom module for Divi Builder/Divi Theme
+Version:     0.1
+Author:      Daniel Bachmann
+Author URI:  https://delusions-of-grandeur.de
+Text Domain: dbwp_divi_extras
+Domain Path: /languages
+License:     GPL3
+
+Custom Module for Divi Builder is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Custom Module for Divi Builder is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Custom Module for Divi Builder. If not, see https://www.gnu.org/licenses/gpl-3.0.
+*/
